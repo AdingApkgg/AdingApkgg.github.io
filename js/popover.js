570d948963284e21a2e6bb670eec5be7447866c7
adingapkgg <adingapkgg@outlook.com>
2023-02-05 20:59:34 +0800

    Site updated: 2023-02-05 20:59:34

---
+++ js/popover.js
@@ -0,0 +1,18 @@
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "哎嘿！复制成功🍬",
+                    message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
