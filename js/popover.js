a8ef985bfd01d5fa9af3d228edaaabd14902619d
adingapkgg <adingapkgg@outlook.com>
2023-02-07 05:41:57 +0800

    Site updated: 2023-02-07 05:41:56

--- js/popover.js
+++ js/popover.js
@@ -16,3 +16,26 @@ document.addEventListener("copy", function () {
         })
     }, 300);
 })
+
+/* F12按键提醒 */
+document.onkeydown = function () {
+    if (window.event && window.event.keyCode == 123) {
+        event.keyCode = 0;
+        event.returnValue = true;
+        debounce(function () {
+            new Vue({
+                data: function () {
+                    this.$notify({
+                        title: "你已被发现😜",
+                        message: "小伙子，扒源记住要遵循GPL协议！",
+                        position: 'top-left',
+                        offset: 50,
+                        showClose: true,
+                        type: "warning",
+                        duration: 5000
+                    });
+                }
+            })
+        }, 300);
+    }
+};
