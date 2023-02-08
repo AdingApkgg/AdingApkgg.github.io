a3a84b45502f1a56ce08e81791c0f06cf3820982
adingapkgg <adingapkgg@outlook.com>
2023-02-09 02:54:59 +0800

    Site updated: 2023-02-09 02:54:58

---
+++ js/title.js
@@ -0,0 +1,17 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = '👀跑哪里去了~';
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = '🐖抓到你啦～';
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
