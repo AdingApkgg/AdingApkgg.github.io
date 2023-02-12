9a0db61dfd0694b7e099b6fd76e8df89902d8123
adingapkgg <adingapkgg@outlook.com>
2023-02-13 04:06:18 +0800

    Site updated: 2023-02-13 04:06:18

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "FF9900",
+      "tmpSize": "16",
+      "cityColor": "CCCCCC",
+      "citySize": "16",
+      "aqiColor": "D9D9D9",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "15px 10px 10px 20px",
+      "shadow": "0",
+      "language": "auto",
+      "borderRadius": "5",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "...."
+    }
+  }
