954a3d7fdf88257f6b60cb12c688d46c00a22346
adingapkgg <adingapkgg@outlook.com>
2023-02-12 21:37:12 +0800

    Site updated: 2023-02-12 21:37:11

--- js/console.js
+++ js/console.js
@@ -1,7 +1,7 @@
 var now1 = new Date();
 
 function createtime1() {
-    var grt = new Date("01/26/2023 00:00:00"); //此处修改你的建站时间或者网站上线时间
+    var grt = new Date("01/06/2023 00:00:00"); //此处修改你的建站时间或者网站上线时间
     now1.setTime(now1.getTime() + 250);
     var days = (now1 - grt) / 1000 / 60 / 60 / 24;
     var dnum = Math.floor(days);
