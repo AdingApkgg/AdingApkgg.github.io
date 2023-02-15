72693433123de0b9b75cc4fc70377a173b98399f
adingapkgg <adingapkgg@outlook.com>
2023-02-16 02:39:14 +0800

    Site updated: 2023-02-16 02:39:13

--- js/popover.js
+++ js/popover.js
@@ -1,1 +1,41 @@
-document.addEventListener("copy",(function(){debounce((function(){new Vue({data:function(){this.$notify({title:"哎嘿！复制成功🍬",message:"若要转载最好保留原文链接哦，给你一个大大的赞！",position:"top-left",offset:50,showClose:!0,type:"success",duration:5e3})}})}),300)})),document.onkeydown=function(){window.event&&123==window.event.keyCode&&(event.keyCode=0,event.returnValue=!0,debounce((function(){new Vue({data:function(){this.$notify({title:"你已被发现😜",message:"小伙子，扒源记住要遵循GPL协议！",position:"top-left",offset:50,showClose:!0,type:"warning",duration:5e3})}})}),300))};
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "哎嘿！复制成功🍬",
+                    message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
+
+/* F12按键提醒 */
+document.onkeydown = function () {
+    if (window.event && window.event.keyCode == 123) {
+        event.keyCode = 0;
+        event.returnValue = true;
+        debounce(function () {
+            new Vue({
+                data: function () {
+                    this.$notify({
+                        title: "你已被发现😜",
+                        message: "小伙子，扒源记住要遵循GPL协议！",
+                        position: 'top-left',
+                        offset: 50,
+                        showClose: true,
+                        type: "warning",
+                        duration: 5000
+                    });
+                }
+            })
+        }, 300);
+    }
+};
