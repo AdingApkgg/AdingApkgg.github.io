4c059d245f6c9d3bf0f20d074920efafefa27923
adingapkgg <adingapkgg@outlook.com>
2023-03-06 02:21:08 +0800

    Site updated: 2023-03-06 02:21:07

--- js/anti-shaking.js
+++ js/anti-shaking.js
@@ -1,1 +1,7 @@
-let TT=null;function debounce(T,e){null!==TT&&clearTimeout(TT),TT=setTimeout(T,e)}
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
